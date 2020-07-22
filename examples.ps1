dd8361121ac50995e24fbd0afdbcab383acfa30b
BobBoba <vlad@vsorokin.com>
2020-07-22 15:46:17 +0200

    added example of closing all open orders on an account

--- examples.ps1
+++ examples.ps1
@@ -13,15 +13,50 @@ if([System.IO.File]::Exists("$PSScriptRoot/settings.ps1")) {
 }
 
 ########################### main code ##########################
-
 $auth_headers = authAsClient -clientId $clientId -clientSecret $clientSecret
 
-'==> calling webapi...'
+# for more information about WebAPI methods and parameters please have a look at https://mywebapi.com/swagger/index.html
 
-# simple ping of MT4 server
+
+############ uncomment needed part of code #######
+
+
+### simple ping of MT4 server
 rest_get "/api/TradePlatforms/$tradePlatformId/Ping"
 
 
+### create balance operation (deposit $100 to acount `1000`)
+# rest_post -url "/api/MT4/$tradePlatformId/TradeTransaction" -data "{'tradeTransactionType':'BrBalance', 'tradeCommand':'Balance', 'orderBy':1000, 'price':100}"
 
-'==> done!'
 
+### get all users
+# rest_get_list "/API/MT4/$tradePlatformId/UsersGet"
+
+
+### get all open orders
+# rest_get_list "/API/MT4/$tradePlatformId/TradesGet"
+
+
+### close all open orders on account `1000`
+#<#
+$login = 1000
+# 1. get user records details
+$userRecord = rest_get "/API/MT4/$tradePlatformId/UserRecordGet/$login"
+$userRecord
+
+# get all open orders for user's group
+$tradeRecords = (rest_get "/API/MT4/$tradePlatformId/AdmTradesRequest/$($userRecord.group)/true").Where({$_.login -eq $login})
+$tradeRecords | Format-Table
+
+foreach ($tr in $tradeRecords) {
+
+    $tradeTransactionType = 'BrClose'
+
+    # pending orders to be deleted, instead of closing
+    if($tr.tradeCommand -in @('BuyLimit', 'SellLimit', 'BuyStop', 'SellStop')){
+        $tradeTransactionType = 'BrDelete'
+    }
+
+    rest_post -url "/api/MT4/$tradePlatformId/TradeTransaction" -data "{'tradeTransactionType':'BrClose', 'tradeCommand':'$($tr.tradeCommand)', 'orderBy':$($tr.login), 'price':$($tr.closePrice), 'symbol':'$($tr.symbol)', 'volume':$($tr.volume), 'order':$($tr.order)}"
+}
+#>
